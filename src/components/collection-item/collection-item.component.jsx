1b154bcb19d8452f825f10c1c10bb24e3fabea7d
Kelvin Robles <kelvin.robles018@gmail.com>
2021-12-23 18:59:34 +0800

    create header component with styles and renamed some scss files

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "./collection-item.style.scss";
+import "./collection-item.styles.scss";
 
 const CollectionItem = ({ name, imageUrl, price }) => (
   <div className="collection-item">
