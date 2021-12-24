fde8e98f42653d48d565d1191b2f11f5f33466b3
Kelvin Robles <kelvin.robles018@gmail.com>
2021-12-24 14:37:59 +0800

    firebase util function for adding non existing user to firebase users entry

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,37 @@ const config = {
     measurementId: "G-YZXXE1ZYP5"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) {
+        return;
+    }
+
+    // DocumentReference for CRUD
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    // DocumentSnapshot with actual DB data
+    const snapShot = await userRef.get();
+
+    // Create `users` entry in firebase if it does not exist
+    if (!snapShot.exists) {
+        const { displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log("error creating user", error.message);
+        }
+    }
+
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
